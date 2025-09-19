8ea5b6863e4dd6b466a184ad81926e965bc94b1c
Elias Gomes <eliasgomesduraes2008@gmail.com>
2025-09-18 22:20:54 -0300

    Herança.

---
+++ 09_Heranca/Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Heranca
+{
+    internal abstract class Pessoa
+    {
+        public string Nome;
+        public string Telefone;
+
+        public abstract string GetDocument();
+
+        public void Imprimir()
+        {
+            Console.WriteLine($"LISTA DE PESSOAS\n\nNome: {this.Nome} Documento: {this.GetDocument()} Telefone: {this.Telefone}");
+        }
+    }
+}
---
+++ 09_Heranca/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Heranca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Não é possivel instanciar uma classe abstrata
+            //Pessoa obj = new Pessoa()
+
+            PessoaFisica pessoaFisica = new PessoaFisica();
+            pessoaFisica.Nome = "Geraldo";
+            pessoaFisica.CPF = "999-999-999-99";
+            pessoaFisica.Telefone = "(14) 99999-9999";
+            pessoaFisica Imprimir();
+
+            PessoaJuridica pessoaJuridica = new PessoaJuridica();
+            pessoaJuridica.Nome = "Senac Marília";
+            pessoaJuridica.CNPJ = "999-999-999-99";
+            pessoaJuridica.Telefone = "(14) 99999-9999";
+            pessoaJuridica Imprimir();
+        }
+    }
+}
